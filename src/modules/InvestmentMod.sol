f237daf972747da5170821b688f208fe53a230de
David Dada <daveproxy80@gmail.com>
2026-02-19 20:18:50 +0100

    Rearrange and sort with comments

--- src/modules/InvestmentMod.sol
+++ src/modules/InvestmentMod.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC4626} from "solady/tokens/ERC4626.sol";
 import {OwnableRoles} from "solady/auth/OwnableRoles.sol";
+import {ERC4626} from "solady/tokens/ERC4626.sol";
 
 error ZeroAddress();
 
@@ -13,6 +13,10 @@ contract InvestmentMod is ERC4626, OwnableRoles {
     uint256 private constant MIN_DURATION = 365 days;
     uint256 public constant ADMIN_ROLE = 1;
     uint256 public constant INVESTOR_ROLE = 2;
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                            BOND SHARES METADATA
+    ///////////////////////////////////////////////////////////////////////////*/
 
     /// @dev Returns the name of the token.
     function name() public view virtual override returns (string memory) {
@@ -29,15 +33,9 @@ contract InvestmentMod is ERC4626, OwnableRoles {
         return USDC;
     }
 
-    /// @dev Returns the number of decimals of the underlying asset.
-    function _underlyingDecimals() internal view virtual override returns (uint8) {
-        return USDC_UNDERLYING_DECIMALS;
-    }
-
-    /// @dev Returns the decimals offset.
-    function _decimalsOffset() internal view virtual override returns (uint8) {
-        return DECIMALS_OFFSET;
-    }
+    //*//////////////////////////////////////////////////////////////////////////
+    //                             BOND SHARES HOOKS
+    //////////////////////////////////////////////////////////////////////////*//
 
     /// @dev Hook that is called before any withdrawal or redemption.
     function _beforeWithdraw(uint256, uint256) internal virtual override {
@@ -46,6 +44,10 @@ contract InvestmentMod is ERC4626, OwnableRoles {
 
     /// @dev Hook that is called after any deposit or mint.
     function _afterDeposit(uint256 assets, uint256 shares) internal virtual override {}
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                                 OVERRIDES
+    //////////////////////////////////////////////////////////////////////////*//
 
     function approve(address spender, uint256 amount) public virtual override returns (bool) {
         if (spender == address(0)) revert ZeroAddress();
@@ -60,5 +62,15 @@ contract InvestmentMod is ERC4626, OwnableRoles {
     function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
         if (from == address(0) || to == address(0)) revert ZeroAddress();
         return super.transferFrom(from, to, amount);
+    }
+
+    /// @dev Returns the number of decimals of the underlying asset.
+    function _underlyingDecimals() internal view virtual override returns (uint8) {
+        return USDC_UNDERLYING_DECIMALS;
+    }
+
+    /// @dev Returns the decimals offset.
+    function _decimalsOffset() internal view virtual override returns (uint8) {
+        return DECIMALS_OFFSET;
     }
 }
